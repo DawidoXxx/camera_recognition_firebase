99bcbd8a003953c9f6c7d75279fd76324fda768c
DawidoXxx <zygadloda@gmail.com>
2023-02-01 16:44:22 +0100

    login and register activity and layout done

--- src/main/java/com/seleco/camera_recognition_firebase/ui/LoginActivity.kt
+++ src/main/java/com/seleco/camera_recognition_firebase/ui/LoginActivity.kt
@@ -1,12 +1,60 @@
 package com.seleco.camera_recognition_firebase.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import com.seleco.camera_recognition_firebase.R
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var loginButton:Button
+    private lateinit var passwordInput:TextView
+    private lateinit var emailInput:TextView
+    private lateinit var registerButton:Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login2)
+
+        loginButton = findViewById(R.id.loginBtn)
+        emailInput = findViewById(R.id.email_text_input)
+        passwordInput = findViewById(R.id.password_text_input)
+        registerButton = findViewById(R.id.register_btn)
+
+        mAuth = FirebaseAuth.getInstance()
+
+        loginButton.setOnClickListener {
+            val email = emailInput.text.toString()
+            val password = passwordInput.text.toString()
+            signInWithEmailAndPassword(email, password)
+        }
+
+        registerButton.setOnClickListener {
+            startActivity(Intent(this,RegisterActivity::class.java))
+        }
     }
+
+    private fun signInWithEmailAndPassword(email: String, password: String) {
+        mAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    //we can now get some info about user
+                    val user = mAuth.currentUser
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                } else {
+                    Log.w("LoginActivity", "signInWithEmailAndPassword:failure", task.exception)
+                    Toast.makeText(this, "Autentykacja nie powiodła się.", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+    //to logout
+    //Firebase.auth.signOut()
 }
--- src/main/java/com/seleco/camera_recognition_firebase/ui/RegisterActivity.kt
+++ src/main/java/com/seleco/camera_recognition_firebase/ui/RegisterActivity.kt
@@ -1,12 +1,65 @@
 package com.seleco.camera_recognition_firebase.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import com.seleco.camera_recognition_firebase.R
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var registerBtn:Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        auth = FirebaseAuth.getInstance()
+
+        registerBtn.setOnClickListener {
+            val email = findViewById<TextView>(R.id.email_register_text_input).text.toString().trim()
+            val password = findViewById<TextView>(R.id.password_register_text_input).text.toString().trim()
+            val passwordAgain = findViewById<TextView>(R.id.password_again_register_text_input).text.toString().trim()
+
+            if (!password.equals(passwordAgain)) {
+                Toast.makeText(this, "Passwords are different", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            if (TextUtils.isEmpty(email)) {
+                Toast.makeText(this, "Enter email address!", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            if (TextUtils.isEmpty(password)) {
+                Toast.makeText(this, "Enter password!", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            if (password.length < 6) {
+                Toast.makeText(this, "Password too short, enter minimum 6 characters!", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            //should be there way to main activity or go to login?
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Log.d("FirebaseAuth", "createUserWithEmail:success")
+                        val user = auth.currentUser
+                        startActivity(Intent(this, MainActivity::class.java))
+                        finish()
+                    } else {
+                        Log.w("FirebaseAuth", "createUserWithEmail:failure", task.exception)
+                        Toast.makeText(this, "Authentication failed.", Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
     }
 }
